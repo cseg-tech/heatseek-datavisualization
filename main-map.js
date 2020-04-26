8ce8c8c16ad5a7e3323a685b2418cbf7054584d9
nm3076 <nm3076@columbia.edu>
2020-04-27 00:23:27 +0800

    Files changed to accomodate for new json

--- main-map.js
+++ main-map.js
@@ -21,7 +21,7 @@ var violation = {
 }
 
 // Plot map
-d3.json("map-data-severity.json", function(data) {
+d3.json("hs-severity.json", function(data) {
 
   d3.select("#mapid")
     .select("svg")
@@ -29,18 +29,18 @@ d3.json("map-data-severity.json", function(data) {
     .data(data)
     .enter()
     .append("circle")
-      .attr("cx", function(d){ return map.latLngToLayerPoint([d.lat, d.lon]).x })
-      .attr("cy", function(d){ return map.latLngToLayerPoint([d.lat, d.lon]).y })
+      .attr("cx", function(d){ return map.latLngToLayerPoint([d.la, d.lo]).x })
+      .attr("cy", function(d){ return map.latLngToLayerPoint([d.la, d.lo]).y })
       .attr("r", function(d) {
-        if(d.severity == 0) {
-          return (d.severity /3) + 5;  
+        if(d.s == 0) {
+          return (d.s /3) + 5;  
         }
         else {
-          return (d.severity ** (1/3)) * 4;
+          return (d.s ** (1/3)) * 4;
         }
       }) // change this to correlate with severity
       .style("fill", function(d) {
-        return violation[d.violation];
+        return violation[d.v];
       })
       .attr("stroke", false)
       .attr("fill-opacity", .8)
@@ -48,8 +48,8 @@ d3.json("map-data-severity.json", function(data) {
   // Function that update circle position if something change
   function update() {
     d3.selectAll("circle")
-      .attr("cx", function(d){ return map.latLngToLayerPoint([d.lat, d.lon]).x })
-      .attr("cy", function(d){ return map.latLngToLayerPoint([d.lat, d.lon]).y })
+      .attr("cx", function(d){ return map.latLngToLayerPoint([d.la, d.lo]).x })
+      .attr("cy", function(d){ return map.latLngToLayerPoint([d.la, d.lo]).y })
   }
 
   // If the user change the map (zoom or drag), update circle position:
